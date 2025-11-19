129e2b522b56a93b7aee4abecd66e0e1d40af23f
KARINDA Bertin <140190760+kabertin@users.noreply.github.com>
2025-11-19 21:21:39 +0000

    mastering layout

--- Dashboard/src/App.jsx
+++ Dashboard/src/App.jsx
@@ -1,47 +1,15 @@
 import React, { useState } from 'react';
-import { Typography, Card, CardContent, CardActions, Stack, Container, Button, Checkbox } from '@mui/material';
+import { Typography, Card, CardContent, CardActions, Stack, Container, Button, Checkbox, Grid } from '@mui/material';
 import { Welcome } from './Welcome.jsx'; // Named import
 
 export default function App() {
   const [clicks, setClicks] = useState(10);
 
   return (
-    <Container sx={{ textAlign: 'center', mt: 5 }}>
-      <Card>
-        <CardContent>
-          <Typography variant="h2">Title</Typography>
-          <Typography variant="body1">Description goes here.</Typography>
-        </CardContent>
-        <CardActions>
-          <Button>Learn More</Button>
-        </CardActions>
-      </Card>
-      <Typography variant="h2" color="primary" gutterBottom>
-        Hello World from a Component!
-      </Typography>
-      <Typography variant="h1">This is H1</Typography>
-      <Typography variant="h4" color="primary">Primary Heading</Typography>
-      <Typography variant="body1">This is normal body text.</Typography>
+    <Grid container spacing={2}>
+  <Grid item xs={12} sm={6} md={4}>Card</Grid>
+  <Grid item xs={12} sm={6} md={4}>Card</Grid>
+</Grid>
 
-      <Welcome name="Bertin" />
-
-      <Button
-        variant="contained"
-        color="secondary"
-        sx={{ mt: 3 }}
-        onClick={() => setClicks(clicks + 3)}
-      >
-        Click Me
-      </Button>
-      <Checkbox  defaultChecked size="small" />
-
-      <Typography variant="h5" sx={{ mt: 2 }}>
-        Button clicked {clicks} times
-      </Typography>
-      <Stack spacing={1} direction="row">
-        <Button variant="outlined">One</Button>
-        <Button variant="contained">Two</Button>
-      </Stack>
-    </Container>
   );
 }
